261d8c0965c9f1b9e4c86ea857d137f590aa136a
Victorio <vikinik01@abv.bg>
2024-02-09 12:51:50 +0200

    Moving Dockerfile one level up.

--- OpenShiftDemoApp/Controllers/WeatherForecastController.cs
+++ OpenShiftDemoApp/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using OpenShiftDemoApp.Models;
 
 namespace OpenShiftDemoApp.Controllers;
 
@@ -22,11 +26,20 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+    
+    [HttpGet("/users")]
+    public async Task<IActionResult> Users()
+    {
+        await using var fileStream = System.IO.File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "users.json"));
+        var users = await JsonSerializer.DeserializeAsync<List<User>>(fileStream);
+
+        return Ok(users);
     }
 }
--- OpenShiftDemoApp/Program.cs
+++ OpenShiftDemoApp/Program.cs
@@ -1,25 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace OpenShiftDemoApp;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
