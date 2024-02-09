09ba51b6d9a0761aaf502b1b1c992e4d41bd2dd9
Victorio <vikinik01@abv.bg>
2024-02-09 14:55:07 +0200

    Removing HTTPS redirection.

--- OpenShiftDemoApp/Program.cs
+++ OpenShiftDemoApp/Program.cs
@@ -19,7 +19,7 @@ public static class Program
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
+        // app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
 
